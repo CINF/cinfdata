62fb0837a8bf806f7f4ae1603b8026668c66f949
Robert Jensen <jensen.robert@gmail.com>
2022-07-08 09:12:57 +0000

    Better handling og missing  array keys.

--- sym-files2/export_data.php
+++ sym-files2/export_data.php
@@ -15,18 +15,19 @@ foreach (array('diff_left_y', 'linscale_left_y2', 'linscale_left_y1',
 	       'linscale_x1', 'linscale_x2', 'linscale_right_y0',
 	       'linscale_right_y1', 'linscale_right_y2',
 	       'diff_right_y') as $value){
-  $boolean_options .= ',' . $value . ':' . $_GET[$value];
+  $boolean_options .= ',' . $value . ':' . ($_GET[$value] ?? False);
 }
 
 # Scales
 $left_yscale_bounding = $_GET['left_ymin'] . ',' . $_GET['left_ymax'];
 $right_yscale_bounding = $_GET['right_ymin'] . ',' . $_GET['right_ymax'];
-$xscale_bounding = $_GET['xmin'] . ',' . $_GET['xmax'];
+$xscale_bounding = ($_GET['xmin'] ?? '') . ',' . ($_GET['xmax'] ?? '');
 # Plotlists
 $left_plotlist = ''; $right_plotlist = '';
 foreach (array('left_plotlist', 'right_plotlist') as $list){
   $$list = '';
-  if (count($_GET[$list]) > 0){
+  $plotlist = $_GET[$list] ?? array();
+  if (count($plotlist) > 0){
     foreach($_GET[$list] as $id){
       $$list .= ',' . $id;
     }
@@ -38,7 +39,8 @@ $from_to  = $_GET['from'] . ',' . $_GET['to'];
 
 ### Plugin settings
 $db = std_db();
-$plugin_settings_json = html_entity_decode($_GET['plugin_settings']);
+$plugin_settings = $_GET['plugin_settings'] ?? '';
+$plugin_settings_json = html_entity_decode($plugin_settings);
 # Form intry in input table and get ID
 
 $query = "INSERT INTO plot_com_in (input) values (:plugin_settings)";
