c2820ebeafeecd5dbdb05228d02c834d48092cac
Robert Jensen <jensen.robert@gmail.com>
2022-10-05 11:16:46 +0200

    Avoid php warnings, use modern sql syntax.

--- links/link.php
+++ links/link.php
@@ -4,14 +4,15 @@ include("../common_functions_v2.php");
 $db = std_db();
 
 $URL = $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-$REFERER = $_SERVER["HTTP_REFERER"];
-$comment = $_POST["comment"];
+$REFERER = $_SERVER["HTTP_REFERER"] ?? '';
+$comment = $_POST["comment"] ?? '';
 
 echo(html_header());
 
 if($_GET["url"] == "checked") {
   $query = "INSERT INTO short_links (url,comment) VALUES (\"" . $REFERER . "\",\"" . $comment . "\")";
-  $result  = mysql_query($query, $db);
+  $db->query($query);
+
   $query = "SELECT max(id) from short_links";
   $max_id = single_sql_value($db, $query, 0);
   echo("<br><b>Your full URL:</b> " . $REFERER . " <br> <b>Can now be reached at:</b> <a href=\"http://www.cinfdata.fysik.dtu.dk/links/link.php?id=" . $max_id . "\">http://www.cinfdata.fysik.dtu.dk/links/link.php?id=" . $max_id . "</a><br>");
